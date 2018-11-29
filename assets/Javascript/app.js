b5c59d85f584c59c1ac91242c1b26edbbdc553d9
AChapman <achapman77@gmail.com>
2018-11-29 11:01:18 -0700

    JS for toggling favorite heart

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,8 +1,19 @@
 $(function(){
+    
+    
+    
+    
+        $("#master-table").on("click", "#favorite", function () {
+            
+            var favorite = $("#favorite")
 
-
-
-
+            if (favorite.val() === "false") {
+                favorite.removeClass("far").addClass("fas").val("true");
+            } else {
+                favorite.removeClass("fas").addClass("far").val("false");
+            }
+        })
+    
 
 
 
