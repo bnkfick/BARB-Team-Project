9d43bba9cfbfb2d7d2bfd32fcb01221e98779a02
Barbara Fick <bkantzos@yahoo.com>
2018-12-03 23:44:44 -0700

    working on rank table sort

--- assets/Javascript/btns.js
+++ assets/Javascript/btns.js
@@ -59,18 +59,18 @@ $("#preferences").on("click", "#wind-speed-filter-btn", function () {
                 if (speeds.length > 2) {
                     maxSpeed = speeds[2];
                 }
-                
+
                 //THIS IS THE CONDITION FOR SHOW AND HIDE
                 //@TODO combine these 2.  i just can't think through it right now
                 //if ( maxSpeed && maxSpeed > userWindInput || minSpeed > userWindInput ) {
-                if ( minSpeed > userWindInput ) {
+                if (minSpeed > userWindInput) {
                     if ($(this).attr("data-hide") === "false" || $(this).attr("data-hide") === undefined) {
                         $("#" + mtnId).hide();
                     } else if ($(this).attr("data-hide") === "true") {
                         $("#" + mtnId).show();
                     }
                 }
-                if ( maxSpeed && maxSpeed > userWindInput ) {
+                if (maxSpeed && maxSpeed > userWindInput) {
                     if ($(this).attr("data-hide") === "false" || $(this).attr("data-hide") === undefined) {
                         $("#" + mtnId).hide();
                     } else if ($(this).attr("data-hide") === "true") {
@@ -102,10 +102,47 @@ $("#preferences").on("click", "#wind-speed-filter-btn", function () {
 $("#preferences").on("click", "#rank-filter-btn", function () {
     event.preventDefault();
     console.log("RANK FILTER");
+    var allTables = $('#table-list > table');
+    console.log(allTables);
+    
+    var switching, x, y, mtnIdx, mtnIdy, mtnIdPartsX, mtnIdPartsY, shouldSwitch;
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
 
+        /*Loop through all tables (except the
+        first, which contains table template):*/
+        for (var i = 1; i < allTables.length; i++) {
+            console.log(i);
+            // //start by saying there should be no switching:
+            // shouldSwitch = false;
+            // /*Get the two elements you want to compare,
+            // one from current row and one from the next:*/
+            
+            mtnIdx = $(allTables[i]).attr("id");
+            mtnIdy = $(allTables[i + 1]).attr("id");
+            mtnIdPartsX = mtnIdx.split('-');
+            mtnIdPartsY = mtnIdy.split('-');
+            x= mtnIdPartsX[1];
+            y= mtnIdPartsY[1];
+            // //check if the two rows should switch place:
+            // if (x > y) {
+            //     //if so, mark as a switch and break the loop:
+            //     shouldSwitch = true;
+            //     break;
+            // }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            allTables[i].parentNode.insertBefore(allTables[i + 1], allTables[i]);
+            switching = true;
+        }
+    }
 
-    var mtns = $('.distance');
-    console.log(mtns);
     // var mtnByRank = peakInfo.filter(function (question) {
     //     return question.asked === false;
     // })
@@ -122,7 +159,7 @@ $("#preferences").on("click", "#rank-filter-btn", function () {
     //   var sortedPeakInfo = peakInfo;
     //   sortedPeakInfo.sort(compare);
     //   //console.log("sortedPeakInfo", sortedPeakInfo);
-    var mtnRanks = $('rank');
+    //var mtnRanks = $('rank');
 });
 
 $("#preferences").on("click", "#distance-filter-btn", function () {
